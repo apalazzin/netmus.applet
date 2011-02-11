77c8f678fffea621fa76ca62bba73ae215b75925
skyled <skyled@alice.it>
2011-02-11 18:01:23 +0100

    NEW applet.TranslateXML.java creata la nuova classe per la traduzione in XML.

---
+++ src/applet/TranslateXML.java
@@ -0,0 +1,9 @@
+package applet;
+
+import org.farng.mp3.AbstractMP3Tag;
+
+public class TranslateXML {
+	public TranslateXML(){};
+	public void addMP3(AbstractMP3Tag brano){};
+	public String toString(){return null;};
+}
