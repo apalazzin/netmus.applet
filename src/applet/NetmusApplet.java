b05eb3f2d815fe91679514484feed4c66cea900c
andrea mandolo <andrea.mandolo@gmail.com>
2011-02-18 19:00:32 +0100

    ADD - NetmusApplet.java, DeviceScanner.java - Create Versioni integrabili GWT.

--- src/applet/NetmusApplet.java
+++ src/applet/NetmusApplet.java
@@ -1,68 +1,37 @@
 package applet;
 
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.applet.AppletContext;
+import java.net.URL;
 
 import javax.swing.JApplet;
-import javax.swing.JButton;
-import javax.swing.JTextField;
 
 @SuppressWarnings("serial")
 public class NetmusApplet extends JApplet {
    
-   // Scanner state for the applet
-   private boolean isActive;
-   private String loggedUser;
-   private static DeviceScanner scanner;
-   
-   
-   String s = "";// da togliere
-   JTextField l;
-   JButton button;
-   
-   // rimuovere -> non ci sara' gui
-   @Override
-   public void init() {
-      l = new JTextField(20);// da togliere
-      button = new JButton("Disattiva");// da togliere
-      
-      getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER));// da togliere dopo
-      getContentPane().add(l); // da togliere dopo
-      
-      // bottone per simulare on/off da GWT
-      button.addActionListener(new ActionListener() {// da togliere dopo
-         @Override
-         public void actionPerformed(ActionEvent e) {
-            // TODO Auto-generated method stub
-            setState(!isActive);
-            if(isActive) {
-               button.setText("Disattiva");
-            }
-            else {
-               button.setText("Attiva");
-            }
-            repaint();
-         }
-      });
-      getContentPane().add(button);// da togliere dopo
-   }
-   
+   private boolean isActive;// Scanner state for the applet
+   private String loggedUser; // loggedUser in GWT
+   private static DeviceScanner scanner; // scanner
+   AppletContext app_context;
+
    @Override
    public void start() {
-      
-//      CON QUESTA RICHIESTA VOGLIO ANCHE IL NOME UTENTE, NON SOLO LO STATO
-      isActive = true; // togliere
-      loggedUser = "smile"; // togliere
-//      try {
-//         getAppletContext().showDocument(new URL("javascript:getState()"));
-//      } catch (Exception e) {
-//         e.printStackTrace();
-//      }
-      
-      scanner = new DeviceScanner(loggedUser);
-      setState(isActive);
-      scanner.start();
+       
+       app_context = getAppletContext();
+
+       try {
+           app_context.showDocument(
+                   new URL("javascript:getStarts()"));
+       } catch (Exception e) {
+       }
+   }
+   
+   public void letsGO(String user, boolean state) {
+
+       isActive = state;
+       loggedUser = user;
+       scanner = new DeviceScanner(loggedUser, app_context);
+       setState(isActive);
+       scanner.start();
    }
    
    // metodo chiamato da GWT, contenente lo stato dell'applet
@@ -73,22 +42,10 @@ public class NetmusApplet extends JApplet {
       // da eliminare dopo
       // solo per visualizzare lo stato graficamente
       if (isActive) {
-         s = "   true";
          synchronized(scanner) {
             scanner.notify();
          }
       }
-      else {
-         s = "   false";
-      }
-      l.setText(s);
-      repaint();
-
-//      if (isActive)
-//         fai partire il thread scanner; (notify)
-//      else
-//         ferma il thread scanner (wait)
-      
    }
    
 }
